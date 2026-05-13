851396e6e8d0fa79fc59fd6d1c829e33c51d3640
Parth Khunt <parthkhunt12@gmail.com>
2026-05-13 19:39:57 +0530

    change for Additional scan option

--- resources/views/patients/add_patient.blade.php
+++ resources/views/patients/add_patient.blade.php
@@ -4673,7 +4673,7 @@ async function loadPLYLower(url) {
                 // } else {
                 //     redirectUri = "{{ url('/patient/edit') }}/" + hashCode;
                 // }
-                redirectUri = "{{ url('/patient/edit') }}/" + hashCode + "type=additional";
+                redirectUri = "{{ url('/patient/edit') }}/" + hashCode + "?type=additional";
                 const shining3dAuthUrl =
                     'https://dental3dcloud.com/p/index?' +
                     'codeChallenge={{ config("shining3d.shining3d_code_challenge") }}' +
