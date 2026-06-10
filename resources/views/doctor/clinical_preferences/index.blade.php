8074ad73a984f353330811851c4f13cce9a4e0f9
Parth Khunt <parthkhunt12@gmail.com>
2026-06-10 20:42:03 +0530

    change for clinical-preferences

--- resources/views/doctor/clinical_preferences/index.blade.php
+++ resources/views/doctor/clinical_preferences/index.blade.php
@@ -2,7 +2,21 @@
 
 @section('content')
 <div class="page-content">
-    @include('layouts.breadcrumb')
+    {{-- @include('layouts.breadcrumb') --}}
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-flex flex-column mt-3">
+                <h4 class="page-title mb-0 font-size-18" >Clinical Preferences</h4>
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="{{url('/home')}}">Dashboard</a></li>
+                        <li class="breadcrumb-item active">Clinical Preferences</li>
+                    </ol>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
     <div class="row">
         <div class="col-12">
