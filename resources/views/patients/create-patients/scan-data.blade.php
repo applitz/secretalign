d50d1bd47f396e0a9b9abe4bda651e13656a45fa
Parth Khunt <parthkhunt12@gmail.com>
2026-06-15 17:57:05 +0530

    change for new funcnalites

--- resources/views/patients/create-patients/scan-data.blade.php
+++ resources/views/patients/create-patients/scan-data.blade.php
@@ -355,7 +355,7 @@
         <div class="mb-3 ">
             <a class="btn btn-primary order-from-button"
                 @if(Auth::user()->three_shape_access_token != null)
-                    href="javascript:void(0);" id="select-from-3shape"
+                    href="javascript:void(0);" id="optional-select-from-3shape"
                 @else
                     href="{{url('/integration-3shape/obtain-authorization-code')}}"
                 @endif
@@ -369,7 +369,7 @@
 
             <a class="btn btn-primary order-from-button"
                     @if(Auth::user()->medit_link_access_token != null)
-                            href="javascript:void(0);" id="select-from-medit-link"
+                            href="javascript:void(0);" id="select-from-medit-link-additional"
                     @else
                         href="{{url('/integration-medit-link/obtain-authorization-code')}}"
                     @endif
