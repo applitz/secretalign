e02859f7a2bae1278d1347e07aaf21196ce0c2d6
Parth Khunt <parthkhunt12@gmail.com>
2026-06-16 10:55:29 +0530

    change for new funcnalites

--- config/movixtech.php
+++ config/movixtech.php
@@ -2,6 +2,6 @@
 
 return [
     'email'    => env('MOVIXTECH_EMAIL', 'info@secretalign.com'),
-    'password' => env('MOVIXTECH_PASSWORD', 'IKU&JY^%T$Deyrutiy7ou8iy7tRU%^E2342duiUYT^R%UE^UR7'),
+    'password' => env('MOVIXTECH_PASSWORD', 'LI&%DY$tweyf6rug7it8y9p7t6r5e4wdE&FR*GTHY(PJU*Y)H&T(^R5e4'),
     'url'      => env('MOVIXTECH_URL', 'https://api.movixtech.com'),
 ];
