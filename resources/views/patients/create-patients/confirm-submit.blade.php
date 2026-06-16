4961b9732a6b36370b5ec335ca6d9e771a32c307
Parth Khunt <parthkhunt12@gmail.com>
2026-06-16 16:39:53 +0530

    change for new funcnalites

--- resources/views/patients/create-patients/confirm-submit.blade.php
+++ resources/views/patients/create-patients/confirm-submit.blade.php
@@ -65,6 +65,48 @@
         @if($patient->phase >=2 && $patient->is_submitted == 0)
         <div class="mb-3">
             <div class="container-fluid px-0">
+
+                <div class="mb-3">
+                    <h5 class="text-600 fs-0 mb-2">Select your preferred package.</h5>
+                    <div class="d-flex flex-sm-row  flex-column alert alert-info">
+                        <div class="me-sm-3">
+                            <div class="form-check mb-0 custom-radio radio-select">
+                                <input class="form-check-input" id="pricing_package_1" type="radio" value="select" name="pricing_package" checked="checked" />
+                                <label class="form-check-label mb-0 fw-bold d-block" for="pricing_package_1"> SECRET SELECT </label>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="form-check mb-0 custom-radio radio-select">
+                                <input class="form-check-input" id="pricing_package_2" type="radio" value="confidence" name="pricing_package" />
+                                <label class="form-check-label mb-0 fw-bold d-block" for="pricing_package_2">SECRET CONFIDENCE
+                                    </label>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+
+                 <div class="mb-3">
+                    <h5 class="text-600 fs-0 mb-2">Select your preferred setup type</h5>
+                    <div class="d-flex flex-sm-row  flex-column alert alert-info">
+                        <div class="me-sm-3">
+                            <div class="form-check mb-0 custom-radio radio-select">
+                                <input class="form-check-input" id="setup_type_1" type="radio" value="1" name="setup_type" checked="checked" />
+                                <label class="form-check-label mb-0 fw-bold d-block" for="setup_type_1">
+                                    Final Setup with individual staging  <i class="fas fa-info-circle me-2" data-bs-toggle="tooltip" title="Please note that this treatment Setup and Staging will be meticulously crafted by our expert technicians and tailored to your individual needs to ensure the most predictable and effective results. Due to the extensive clinical work involved in this planning phase, a cancellation fee of €150 will apply if the plan is not approved."></i>
+                                    </label>
+                                </div>
+                        </div>
+                        <div>
+                            <div class="form-check mb-0 custom-radio radio-select">
+                                <input class="form-check-input" id="setup_type_2" type="radio" value="2" name="setup_type" />
+                                <label class="form-check-label mb-0 fw-bold d-block" for="setup_type_2">Quick Setup
+                                    <i class="fas fa-info-circle me-2" data-bs-toggle="tooltip" title="Want an instant preview? Try our Quick AI Setup at no cost. This automated tool provides a fast, preliminary visualization of your potential results, completely free of charge."></i>
+                                </label>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="row">
                     <br>
                     <div class="col-md-12 mb-3">
@@ -166,16 +208,13 @@
             </div>
         </div>
 
-
-
-
-
         <div class="mb-3 ps-2">
                 <div class="form-check">
                 <input class="form-check-input" id="terms_and_conditions" type="checkbox" name="terms_and_conditions" value="1"/>
                 <label class="form-check-label" for="terms_and_conditions">I have read and accepted the <a href="{{asset('public/assets/Pricing-Terms-Conditions.pdf')}}" target="_blank"><b>Packages and Terms & conditions agreement</b></a>.</label>
                 </div>
         </div>
+
         @if($mode == 'edit' && $patient->is_editable == 1)
         <div class="mb-3">
             <button class="btn btn-primary btn-sm waves-effect waves-light px-3 previous-tab" data-target="#pill-tab-li5">Previous</button>
