7c43b2ae3bdf5a557633f639ead1ebf0119d5709
Parth Khunt <parthkhunt12@gmail.com>
2026-08-31 19:19:49 +0530

    change for reupload scan files

--- app/Http/Controllers/Doctor/IntegrationDoctorController.php
+++ app/Http/Controllers/Doctor/IntegrationDoctorController.php
@@ -25,7 +25,7 @@ class IntegrationDoctorController extends Controller
        // dd(env('THREE_SHAPE_API_URI'));
         $previousUrl = url()->previous();
         $redirectData = [ 'url' => $previousUrl, 'type' => 'update-scan', 'patient_id' => null];
-        if (preg_match('/patient\/edit\/([^\/\?]+)/', $previousUrl, $matches)) {
+        if (preg_match('/patient\/upload-new-scan\/([^\/\?]+)/', $previousUrl, $matches)) {
             $redirectData['type'] = 'update-scan';
             $redirectData['patient_id'] = $matches[1];
         }
